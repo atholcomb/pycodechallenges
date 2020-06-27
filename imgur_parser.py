0870aff7fa3be69076a4589dd990912491c00e40
Andrew Holcomb <atholcomb@outlook.com>
2020-06-27 02:16:11 +0000

    refactored

--- imgur_parser.py
+++ imgur_parser.py
@@ -3,21 +3,30 @@
 # imgur_parser.py
 
 def imgurUrlParser(link):
-    if ".png" in link:
-        p = link.index(".png")
-        link = link[:p]
-    if "/a/" in link:
-        a = link.index('a/')
-        return f'(id: "{link[a+2:]}", type: "album")' + ' -> ' + link
+    original_link = link    
+
+    formats = [".png", ".jpg", ".gif"]
+    for f in formats:
+        if f in link:
+            pos_in_format = link.index(f)
+            link = link[:pos_in_format]
+
+    if '/a/' in link:
+        a_slash = link.index('/a/')
+        return f'(id: "{link[a_slash+3:]}", type: "album")' + ' -> ' + link
     elif "gallery" in link:
-        g = link.index('ry')
-        return f'(id: "{link[g+3:]}", type: "gallery")' + ' -> ' + link
+        gal = link.index('gallery')
+        return f'(id: "{link[gal+8:]}", type: "gallery")' + ' -> ' + link
+    elif 'i.imgur' in link:
+        image = link.index('i.imgur')
+        return f'(id: "{link[image+12:]}", type: "image")' + ' -> ' + original_link
     else:
-        i = link.index('m/')
-        return f'(id: "{link[i+2:]}", type: "image")' + ' -> ' + link
+        return f'(id: "{link[17:]}", type: "image")' + ' -> ' + link
 
 
 print(imgurUrlParser("http://imgur.com/a/cjh4E"))
 print(imgurUrlParser("http://imgur.com/gallery/59npG"))
 print(imgurUrlParser("http://imgur.com/OzZUNMM"))
 print(imgurUrlParser("http://i.imgur.com/altd8Ld.png"))
+print(imgurUrlParser("http://i.imgur.com/x0kAx1.jpg"))
+print(imgurUrlParser("http://i.imgur.com/lOpNjuD.gif"))
