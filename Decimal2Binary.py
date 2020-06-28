c24c81a64bcdc87ccd2bc7c7ba89d61f1783615d
Andrew Holcomb <atholcomb@outlook.com>
2020-06-28 11:59:33 +0000

    2nd push

---
+++ Decimal2Binary.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3.7
+# written by: atholcomb
+# Decimal2Binary.py
+
+def Decimal2Binary(n):
+    value = []
+    orignal_n = n
+    
+    while n != 0:
+        if n % 2 == 0:
+            n = n // 2
+            value.append(0) 
+        elif n % 2 == 1:
+            n = n // 2
+            value.append(1)
+        else:
+            break
+
+    return f"// {orignal_n} = {value[::-1]}"
+    
+
+print(Decimal2Binary(10))
+print(Decimal2Binary(12))
+print(Decimal2Binary(25))
+print(Decimal2Binary(45))
