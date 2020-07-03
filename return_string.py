03d855d553e872ba1f866ca7e4a61ca676c92db3
Andrew Holcomb <atholcomb@outlook.com>
2020-07-03 01:57:14 +0000

    3rd push

---
+++ return_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.7
+# written by: atholcomb
+# return_string.py
+
+def return_string(list):
+    result = ''
+
+    for item in list:
+        result += item + ' '
+
+    return result
+
+print(return_string(["hello", "world"]))
+print(return_string(["some", "long", "string"]))
+print(return_string(["today's", "date"]))
+print(return_string(["sunshine"]))
