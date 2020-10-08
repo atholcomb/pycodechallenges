bdd6fbb7deaa0e7277553dbeefab35af8d4a6d15
Andrew Holcomb <atholcomb@outlook.com>
2020-10-08 05:05:41 +0000

    7th push

---
+++ square_patch.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3.7
+# written by: atholcomb
+# square_patch.py
+# Create a function that takes an integer and outputs an n x n square solely consisting of the integer n.
+
+import math
+
+def square_patch(patch_value):
+    patch_list = []
+    
+    if patch_value == 0:
+        return "[]"
+    else:
+        for pv in range(patch_value):
+            patch_list.append(patch_value * 1)
+
+            
+    return f"{patch_list}\n" * patch_value 
+
+
+print(square_patch(3))
+print(square_patch(5))
+print(square_patch(1))
+print(square_patch(0))
+
+# example output
+#square_patch(3) ➞ [
+#  [3, 3, 3],
+#  [3, 3, 3],
+#  [3, 3, 3]
+#]
