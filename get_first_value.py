18510280d9da77796fee56ac6eeebea775512f1d
Andrew Holcomb <atholcomb@outlook.com>
2020-12-16 03:59:23 +0000

    1st push

---
+++ get_first_value.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.7
+# written by: atholcomb
+# get_first_value.py
+# Create a function that takes a list containing only numbers and return the first element.
+
+def get_first_value(a_list):
+    
+    return a_list[0]
+
+
+print(get_first_value([1, 2, 3]))
+print(get_first_value([80, 5, 100]))
+print(get_first_value([-500, 0, 50]))
